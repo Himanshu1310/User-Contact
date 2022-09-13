80cb841e7d1cfc2f1c286d4930bec5bbcda4d4d3
Himanshu1310 <himanshu.rana1310@gmail.com>
2022-09-13 19:53:47 +0530

    'v2'

--- user-service/src/main/java/com/user/entities/User.java
+++ user-service/src/main/java/com/user/entities/User.java
@@ -1,5 +1,63 @@
 package com.user.entities;
 
-public class User {
+import java.util.ArrayList;
+import java.util.List;
 
+public class User {
+	private Long userId;
+	private String name;
+	private int phone;
+	List<Contact> contact = new ArrayList<>();
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	public List<Contact> getContacts() {
+		return contact;
+	}
+
+	public void setContacts(List<Contact> contacts) {
+		this.contact = contacts;
+	}
+
+	public User(Long userId, String name, int phone) {
+		super();
+		this.userId = userId;
+		this.name = name;
+		this.phone = phone;
+	}
+
+	public User(Long userId, String name, int phone, List<Contact> contacts) {
+		super();
+		this.userId = userId;
+		this.name = name;
+		this.phone = phone;
+		this.contact = contacts;
+	}
 }
---
+++ user-service/src/main/java/com/user/service/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.user.service;
+
+import java.util.List;
+
+import com.user.entities.User;
+
+public class UserServiceImpl implements UserService {
+	List<User> list = List.of(
+			new User()
+			);
+			
+
+	@Override
+	public User getUser(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
